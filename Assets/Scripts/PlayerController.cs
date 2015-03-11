861a40bdd429792ebaf99ce0bb94df9ea9009613
neiro <neiro.mail@gmail.com>
2015-03-11 22:35:39 +0300

    Fix style

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour {
 
 	// Player movement
 	void FixedUpdate () {
-  float moveHorizontal = Input.GetAxis ("Horizontal");
-  float moveVertical = Input.GetAxis ("Vertical");
+    float moveHorizontal = Input.GetAxis ("Horizontal");
+    float moveVertical = Input.GetAxis ("Vertical");
 
     Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
