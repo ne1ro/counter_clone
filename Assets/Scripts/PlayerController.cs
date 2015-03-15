3f220594c76495146b12a10b46beb06a0140cfe6
neiro <neiro.mail@gmail.com>
2015-03-15 20:33:51 +0300

    Add shot script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,8 +16,8 @@ public class PlayerController : MonoBehaviour {
       moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
       moveDirection = transform.TransformDirection(moveDirection);
       moveDirection *= speed;
-      if (Input.GetButton("Jump"))
-        moveDirection.y = jumpSpeed;
+
+      if (Input.GetButton("Jump")) moveDirection.y = jumpSpeed;
     }
 
     moveDirection.y -= gravity * Time.deltaTime;
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+  public AudioSource shotSound;
+  public float fireRate = 0.1f;
+
+  private float nextFire = 0.0f;
+
+  void Update () {
+    // Shot
+    if (Input.GetButton("Fire1") && Time.time > nextFire) {
+      nextFire = Time.time + fireRate;
+      shotSound.Play();
+    }
+  }
+}
