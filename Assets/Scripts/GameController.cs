f60f0f440a5f37642ee77cdc76685b47e1d01f71
neiro <neiro.mail@gmail.com>
2015-03-16 21:12:32 +0300

    Lock cursor

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+  void Start() {
+    // Lock cursor
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+}
