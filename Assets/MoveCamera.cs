95f87b7ccf4d4b41bcbc2f09292c98cb822a4c49
neiro <neiro.mail@gmail.com>
2015-03-16 19:28:23 +0300

    Rotate player, not camera

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -4,27 +4,28 @@ using System.Collections;
 public class MoveCamera : MonoBehaviour {
   public float turnSpeed = 4.0f;
   public float panSpeed = 4.0f;
+  public GameObject player;
 
   private Vector3 mouseOrigin;
   private bool isRotating;
 
   void Update() {
     // Get the left mouse button
-    if(Input.GetMouseButtonDown(0)) {
+    if(Input.GetMouseButtonDown(1)) {
       // Get mouse origin
       mouseOrigin = Input.mousePosition;
       isRotating = true;
     }
 
-    // Reset isRotating on mouse click
-    if (!Input.GetMouseButton(0)) isRotating=false;
+    // Reset isRotating on mouse down
+    if (!Input.GetMouseButton(1)) isRotating = false;
 
     // Rotate camera along X and Y axis
     if (isRotating) {
       Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
 
       transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
-      transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+      player.transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
     }
   }
 }
