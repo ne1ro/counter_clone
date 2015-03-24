d2f1c39d12002c0d283d05e07f5f3f91f737de46
neiro <neiro.mail@gmail.com>
2015-03-24 21:18:09 +0300

    Format time

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System;
 
 public class Timer : MonoBehaviour {
   private float startTime;
@@ -10,7 +10,8 @@ public class Timer : MonoBehaviour {
   }
 
   void Update() {
-    float elapsedTime = Time.time - startTime;
-    GetComponent<Text>().text = elapsedTime.ToString();
+    TimeSpan t = TimeSpan.FromSeconds(Time.time - startTime);
+    GetComponent<Text>().text = string.Format("{0:D2}:{1:D2}", t.Minutes,
+      t.Seconds);
   }
 }
