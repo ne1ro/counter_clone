df329f62771b564517b2e5367bb75c2cab2d7afd
neiro <neiro.mail@gmail.com>
2015-03-25 21:09:25 +0300

    Add weapons round count text

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -1,20 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Shot : MonoBehaviour {
-  public AudioClip shotSound;
   public float fireRate = 1.5f; // Battle fire rate is 40 per minute for single shots
   public int cartridgeCount = 30;
   public float chargeInterval = 3.5f;
   public int distance = 1500; // Penetration distance, use 3000 for non-lethal
 
-  private float nextFire = 0.0f;
+  public AudioClip shotSound;
+  public AudioClip chargeSound;
+  public Text roundCountText;
 
-  void Update () {
+
+  private float nextFire;
+  private int roundCount;
+
+  void Start() {
+    nextFire = 0.0f;
+    roundCount = 5;
+    roundCountText.text = roundCount.ToString();
+  }
+
+  void Update() {
+    bool isCharged = roundCount > 0;
     // Shot
-    if (Input.GetButton("Fire1") && Time.time > nextFire) {
+    if (Input.GetButton("Fire1") && Time.time > nextFire && isCharged) {
       nextFire = Time.time + fireRate;
+      roundCount --;
+      roundCountText.text = roundCount.ToString();
       GetComponent<AudioSource>().PlayOneShot(shotSound);
+    }
+
+    // Recharge
+    if (roundCount <= 0) {
+      roundCount = cartridgeCount;
     }
   }
 }
