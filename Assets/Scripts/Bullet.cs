deeeaafda20ebd00d845c9f3e34148a74b006fc9
neiro <neiro.mail@gmail.com>
2015-03-28 11:46:26 +0300

    Destroy bullets on trigger event

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
   public int bulletSpeed = 715;
 
-	void Start () {
+	void Start() {
     GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * bulletSpeed);
 	}
+
+  void OnTriggerEnter(Collider other) {
+    Destroy(gameObject);
+  }
 }
