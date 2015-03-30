a86f70220d0c194c23dd2ce5ff0daf5a8f529ab1
neiro <neiro.mail@gmail.com>
2015-03-30 18:07:50 +0300

    Trace bullet with DrawRay

--- Assets/Scripts/DestroyByCollision.cs
+++ Assets/Scripts/DestroyByCollision.cs
@@ -13,6 +13,11 @@ public class DestroyByCollision : MonoBehaviour {
     gameController = gameControllerObject.GetComponent<GameController>();
 	}
 
+  void Update() {
+    Vector3 forward = transform.TransformDirection(Vector3.forward) * 7;
+    Debug.DrawRay(transform.position, forward, Color.red);
+  }
+
   void OnTriggerEnter(Collider other) {
     if (other.tag == "Mob") {
       Destroy(other.gameObject);
