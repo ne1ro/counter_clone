d4fc09ac62119b0e56f044c267e792619351ad92
neiro <neiro.mail@gmail.com>
2015-03-30 17:27:27 +0300

    Add score  on mob killing

--- Assets/Scripts/DestroyByCollision.cs
+++ Assets/Scripts/DestroyByCollision.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DestroyByCollision : MonoBehaviour {
   public int bulletSpeed = 715;
+  private GameController gameController;
 
 	void Start() {
     GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * bulletSpeed);
+
+    GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+    gameController = gameControllerObject.GetComponent<GameController>();
 	}
 
   void OnTriggerEnter(Collider other) {
     if (other.tag == "Mob") {
       Destroy(other.gameObject);
+      gameController.AddScore(500);
     }
     if (other.tag != "Boundary") {
       Destroy(gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
+  public Text scoreText;
+  private int score = 0;
+
   void Awake() {
     Cursor.visible = true;
     Cursor.lockState = CursorLockMode.Locked;
+    scoreText.text = score.ToString();
+  }
+
+  public void AddScore(int val) {
+    score += val;
+    scoreText.text = score.ToString();
   }
 }
